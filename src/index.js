aa2ab392f7e68b48455a158b7274578c17caa33d
Razvan Stoenescu <razvan.stoenescu@gmail.com>
2019-04-16 19:38:42 +0300

    feat: review

--- src/index.js
+++ src/index.js
@@ -5,29 +5,21 @@
  * API: https://github.com/quasarframework/quasar/blob/master/app/lib/app-extension/IndexAPI.js
  */
 
-function extendWithComponent (api, conf) {
-  // add quasar dependencies
-  if (!conf.framework) conf.framework = {}
-  if (!conf.framework.directives) conf.framework.directives = []
-  if (!conf.framework.directives.includes('TouchPan')) conf.framework.directives.push('TouchPan')
-
-  // add boot file
-  if (!conf.boot) conf.boot = []
-  const boot = conf.boot
-
+function extendWithComponent (conf) {
   // make sure boot file is registered
-  if (!boot.includes('~quasar-app-extension-swipe-to-close/src/boot/index.js')) {
-    boot.push('~quasar-app-extension-swipe-to-close/src/boot/index.js')
-    // make sure boot file transpiles
-    conf.build.transpileDependencies.push(/quasar-app-extension-swipe-to-close[\\/]src[\\/]boot/)
-    console.log(` App Extension (qswipe-to-close) Info: 'Adding swipe-to-close boot reference to your quasar.conf.js'`)
-  }
+  conf.boot.push('~quasar-app-extension-swipe-to-close/src/boot/index.js')
+
+  // make sure boot file transpiles
+  conf.build.transpileDependencies.push(/quasar-app-extension-swipe-to-close[\\/]src/)
+  console.log(` App Extension (qswipe-to-close) Info: 'Adding swipe-to-close boot reference to your quasar.conf.js'`)
 }
 
-module.exports = function (api, ctx) {
+module.exports = function (api) {
+  api.compatibleWith('@quasar/app', '^1.0.0-beta.18')
+
   // register JSON api
   api.registerDescribeApi('QSwipeToClose', './components/QSwipeToClose.json')
 
   // extend quasar.conf
-  api.extendQuasarConf(conf => extendWithComponent(api, conf))
+  api.extendQuasarConf(extendWithComponent)
 }
